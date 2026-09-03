a3e65cc439bdfc0b0d616ba8b4f732b7894e371b
bitmavrick <email.mehedi.bd@gmail.com>
2026-09-03 09:34:51 +0600

    Add Config.kt to buildSrc
    - Create `Config.kt` to centralize project configuration constants.
    - Define `APP_ID`, `VERSION_CODE`, and `VERSION_NAME`.
    - Set SDK versions for compile (37), target (37), and minimum requirements (26).

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,10 @@
+object Config {
+    const val APP_ID = "com.bitmavrick"
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0.0.$VERSION_CODE"
+    const val COMPILE_SDK_VERSION = 37
+    const val TARGET_SDK_VERSION = 37
+    const val MIN_SDK_VERSION = 26 // * Android 8.0 (Oreo)
+
+    // * In case of process error: taskkill /im java.exe /f
+}
