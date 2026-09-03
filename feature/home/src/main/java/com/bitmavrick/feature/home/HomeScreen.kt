b30d35db264b1b16799ab9f74bcabe0acb49d16c
bitmavrick <email.mehedi.bd@gmail.com>
2026-09-03 17:51:00 +0600

    Implement initial HomeScreen in :feature:home module
    - Create `HomeScreen` composable featuring a search trigger and a results list.
    - Integrate `PlayStoreRepositoryImpl` to fetch and display app search results.
    - Implement UI state handling for loading and error scenarios.
    - Update `MainActivity` to render the new `HomeScreen`.

--- feature/home/src/main/java/com/bitmavrick/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/bitmavrick/feature/home/HomeScreen.kt
@@ -1,9 +1,5 @@
-package com.bitmavrick.asopilot
+package com.bitmavrick.feature.home
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +9,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,30 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bitmavrick.asopilot.data.model.PlayStoreApp
-import com.bitmavrick.asopilot.data.repositoryImpl.PlayStoreRepositoryImpl
-import com.bitmavrick.asopilot.ui.theme.ASOPilotTheme
-import com.bitmavrick.feature.home.HomeScreen
+import com.bitmavrick.core.data.repositoryImpl.PlayStoreRepositoryImpl
+import com.bitmavrick.core.model.PlayStoreApp
 import kotlinx.coroutines.launch
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ASOPilotTheme {
-                // App()
-                HomeScreen()
-            }
-        }
-    }
-}
-
-
 @Composable
-fun App(){
+fun HomeScreen() {
     val scope = rememberCoroutineScope()
 
     var isLoading by remember { mutableStateOf(false) }
